79710548a767ad2dc87f806c3c02cadbd21abc4e
Winston Chang <winston@stdout.org>
2014-06-20 11:00:05 -0500

    Fix R CMD check warnings with filter.test method

--- R/test.r
+++ R/test.r
@@ -1,4 +1,11 @@
-#' @importFrom dplyr filter
 #' @method filter test
 #' @export
 filter.test <- function(...) NULL
+
+#' Filter
+#' @param .data Data
+#' @param ... Other args
+#' @name filter
+#' @importFrom dplyr filter
+#' @export filter
+NULL
